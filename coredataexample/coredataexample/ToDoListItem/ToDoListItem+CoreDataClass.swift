1964cd4beaa0f51c7e191a96dde3a8e4b56e7493
Apple <128469441+appledevelopment12@users.noreply.github.com>
2023-04-12 12:03:12 +0530

    Add files via upload

---
+++ coredataexample/coredataexample/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  coredataexample
+//
+//  Created by Rajeev on 11/04/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ coredataexample/coredataexample/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  coredataexample
+//
+//  Created by Rajeev on 11/04/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
